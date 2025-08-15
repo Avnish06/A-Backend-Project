a22d3939baca972ace0d96077bc4aa4bbd81de54
Avnish Kumar <avnishkr.mbd@gmail.com>
2025-08-15 18:38:02 +0530

    This is my 3rd push

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+import DB_NAME
+import express from "express"
+import DB_NAME FROM "./constants.js"
+
+const ConnectDB = async () => {
+
+try {
+
+   const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+   console.log(`\n MongoDB connected  !! DB HOST: ${connectionInstance.connection.host}`)
+    
+} catch (error) {
+    console.log("MONGODB connection error", error)
+    process.exit(1)
+}
+}
+
+
+export default ConnectDB
